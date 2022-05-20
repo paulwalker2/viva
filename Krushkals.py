9570c54591088ad1959511c94102a1d447c7b7a3
Avishkar Hande <64465275+avishkarhande@users.noreply.github.com>
2022-05-20 08:56:56 +0530

    Update Krushkals.py

--- Krushkals.py
+++ Krushkals.py
@@ -1,58 +1,104 @@
 class Graph:
-    def _init_(self, vertex):
-        self.V = vertex
-        self.graph = []
- 
-    def add_edge(self, u, v, w):
+
+    def __init__(self, vertices):
+        self.V = vertices  # No. of vertices
+        self.graph = []  # default dictionary
+
+    # to store graph
+
+    # function to add an edge to graph
+    def addEdge(self, u, v, w):
         self.graph.append([u, v, w])
- 
- 
-    def search(self, parent, i):
+
+    # A utility function to find set of an element i
+    # (uses path compression technique)
+    def find(self, parent, i):
         if parent[i] == i:
             return i
-        return self.search(parent, parent[i])
- 
-    def apply_union(self, parent, rank, x, y):
-        xroot = self.search(parent, x)
-        yroot = self.search(parent, y)
+        return self.find(parent, parent[i])
+
+    # A function that does union of two sets of x and y
+    # (uses union by rank)
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        # Attach smaller rank tree under root of
+        # high rank tree (Union by Rank)
         if rank[xroot] < rank[yroot]:
             parent[xroot] = yroot
         elif rank[xroot] > rank[yroot]:
             parent[yroot] = xroot
+
+        # If ranks are same, then make one as root
+        # and increment its rank by one
         else:
             parent[yroot] = xroot
             rank[xroot] += 1
- 
-  
-    def kruskal(self):
-        result = []
-        i, e = 0, 0
-        self.graph = sorted(self.graph, key=lambda item: item[2])
+
+    # The main function to construct MST using Kruskal's
+    # algorithm
+    def KruskalMST(self):
+
+        result = []  # This will store the resultant MST
+
+        # An index variable, used for sorted edges
+        i = 0
+
+        # An index variable, used for result[]
+        e = 0
+
+        # Step 1: Sort all the edges in
+        # non-decreasing order of their
+        # weight. If we are not allowed to change the
+        # given graph, we can create a copy of graph
+        self.graph = sorted(self.graph,
+                            key=lambda item: item[2])
+
         parent = []
         rank = []
+
+        # Create V subsets with single elements
         for node in range(self.V):
             parent.append(node)
             rank.append(0)
+
+        # Number of edges to be taken is equal to V-1
         while e < self.V - 1:
+
+            # Step 2: Pick the smallest edge and increment
+            # the index for next iteration
             u, v, w = self.graph[i]
             i = i + 1
-            x = self.search(parent, u)
-            y = self.search(parent, v)
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            # If including this edge doesn't
+            # cause cycle, include it in result
+            # and increment the indexof result
+            # for next edge
             if x != y:
                 e = e + 1
                 result.append([u, v, w])
-                self.apply_union(parent, rank, x, y)
+                self.union(parent, rank, x, y)
+        # Else discard the edge
+
+        minimumCost = 0
+        print("Edges in the constructed MST")
         for u, v, weight in result:
-            print("Edge:",u, v,end =" ")
-            print("-",weight)
- 
- 
-g = Graph(5)
-g.add_edge(0, 1, 8)
-g.add_edge(0, 2, 5)
-g.add_edge(1, 2, 9)
-g.add_edge(1, 3, 11)
-g.add_edge(2, 3, 15)
-g.add_edge(2, 4, 10)
-g.add_edge(3, 4, 7)
-g.kruskal()
+            minimumCost += weight
+            print("%d -- %d == %d" % (u, v, weight))
+        print("Minimum Spanning Tree", minimumCost)
+
+
+# Driver code
+g = Graph(4)
+g.addEdge(0, 1, 10)
+g.addEdge(0, 2, 6)
+g.addEdge(0, 3, 5)
+g.addEdge(1, 3, 15)
+g.addEdge(2, 3, 4)
+
+# Function call
+g.KruskalMST()
+
